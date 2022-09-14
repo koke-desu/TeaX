ca41bb6d9a67a2c477d8b116ded28c08d0799b78
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-14 19:48:16 +0900

    typeを更新

--- src/type/model.ts
+++ src/type/model.ts
@@ -1,4 +1,5 @@
 // emailとpasswordはauthで管理。
+
 // idはauthのUUID
 export type User = {
   id: string;
@@ -8,11 +9,12 @@ export type User = {
 // 「バナナスムージー」チョコクリームトッピング。みたいな感じのを表す。
 // nameはデフォルトのメニューにのみ付く。
 export type Menu = {
-  id: string;
-  name?: string;
+  // id: string;
+  name: string | "custom";
   ingredient: Ingredient;
   toppings: Topping[];
-  price: number;
+  sumPrice: number; //トッピングとメイン材料の合計価格
+  coupon: Coupon;
 };
 
 // メインの材料。バナナとか
@@ -33,19 +35,28 @@ export type Topping = {
 };
 
 // クーポンの詳細。中身は未定。割引や隠しメニューとか？
+//typeとnumberで価格の計算を行う。例えば、type:multiple,number:0.7の場合は3割引きを意味し、価格×0.7を計算する。
 export type Coupon = {
   id: string;
   title: string;
   description: number;
+  type: CouponType;
+  number: number;
+  isGetting: boolean;
+  isUsed: boolean;
 };
+//○○割引はmultiple,○○円引きはsubtract,○○無料はfree123でタイプで分ける
+export type CouponType = "multiple" | "subtract" | "free1" | "free2" | "free3";
 
 // Menuと受け取り予定時刻とか。
 export type Order = {
-  id: string;
-  createdAt: Date; //
+  // id: string;
+  orderNumber: number;
+  createdAt: Date;
   pickupTime: Date; // 受け取り予定時刻
-  user: User;
+  isCompleted: boolean; //オーダーしたスムージーが完成したかどうか
+  userId: string;
   menu: Menu[];
-  coupons: Coupon[];
-  totalPrice: number;
+  // coupons: Coupon[];メニューにつき一つクーポンを適用できるようにするためここにクーポンは置かないようにします
+  totalPrice: number; //Menuらの合計価格
 };
