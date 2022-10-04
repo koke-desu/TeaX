7a1902f5fbe3176ae0b51f0afa7b807dfb2394f4
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-05 03:36:40 +0900

    型の変更

--- src/type/model.ts
+++ src/type/model.ts
@@ -3,60 +3,81 @@
 // idはauthのUUID
 export type User = {
   id: string;
-  history: Order[];
+  coupons: { [couponID: string]: couponState };
+  //booleanはスタンプラリーのスポットをクリアし終わっていたらtrueそうでなければfalse
+  quizzes: { [quizID: string]: boolean };
+};
+
+export type couponState = "used" | "unOwned" | "useable";
+
+export type OrderData = {
+  OrderMenus: OrderMenu[];
+  createdAt: Date;
+  isCompleted: boolean;
+  orderKeyword: string;
+  totalPrice: number;
+  userId: string;
 };
 
 // 「バナナスムージー」チョコクリームトッピング。みたいな感じのを表す。
 // nameはデフォルトのメニューにのみ付く。
-export type Menu = {
-  // id: string;
-  name: string | "custom";
-  ingredient: Ingredient;
-  toppings: Topping[];
-  sumPrice: number; //トッピングとメイン材料の合計価格
-  coupon: Coupon;
+export type OrderMenu = {
+  //トッピングIDをリスト形式で格納しデータを参照する時はIDをもとにトッピング一覧データから参照
+  toppings: string[];
+  menuID: string;
+  menuPrice: number; //トッピングとメイン材料の合計価格
+  couponID: string | null;
 };
 
-// メインの材料。バナナとか
-export type Ingredient = {
-  id: string;
-  name: string;
+export type Menus = {
+  [menuID: string]: Menu;
+};
+
+export type Menu = {
   description: string;
+  name: string;
   price: number;
+  //バナナ:falseのような感じで記述するイメージ
+  allergy: { [allergyName: string]: boolean };
 };
 
 // トッピング。複数種類つけることが出来る。
+
+export type Toppings = {
+  [toppingID: string]: Topping;
+};
+
 // ココアパウダー、スライスした果物とか
 export type Topping = {
-  id: string;
   name: string;
   description: number;
   price: number;
+};
+
+export type Coupons = {
+  [couponID: string]: Coupon;
 };
 
 // クーポンの詳細。中身は未定。割引や隠しメニューとか？
 //typeとnumberで価格の計算を行う。例えば、type:multiple,number:0.7の場合は3割引きを意味し、価格×0.7を計算する。
 export type Coupon = {
-  id: string;
   title: string;
   description: number;
   type: CouponType;
   number: number;
-  isGetting: boolean;
-  isUsed: boolean;
 };
+
 //○○割引はmultiple,○○円引きはsubtract,○○無料はfree123でタイプで分ける
 export type CouponType = "multiple" | "subtract" | "free1" | "free2" | "free3";
 
-// Menuと受け取り予定時刻とか。
-export type Order = {
-  // id: string;
-  orderNumber: number;
-  createdAt: Date;
-  pickupTime: Date; // 受け取り予定時刻
-  isCompleted: boolean; //オーダーしたスムージーが完成したかどうか
-  userId: string;
-  menu: Menu[];
-  // coupons: Coupon[];メニューにつき一つクーポンを適用できるようにするためここにクーポンは置かないようにします
-  totalPrice: number; //Menuらの合計価格
+export type quizzes = {
+  [quizID: string]: quiz;
+};
+
+export type quiz = {
+  tips: string;
+  choiceNum: number;
+  explaneImgURL: string;
+  explane: string;
+  answer: number;
 };
