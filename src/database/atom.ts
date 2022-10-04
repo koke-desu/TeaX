63cbae18d24d80e8449fe54e87638776ed3faab6
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-05 03:34:26 +0900

    atomの追加

--- src/database/atom.ts
+++ src/database/atom.ts
@@ -1,11 +1,44 @@
 import { atom, RecoilState } from "recoil";
-import { User } from "../type/model";
+import {
+  Coupons,
+  Menus,
+  quizzes,
+  User,
+  OrderMenu,
+  Toppings,
+} from "../type/model";
 import { auth } from "./firebase";
 
 export const userAtom = atom({
   key: "user",
   default: {
     id: auth.currentUser?.uid,
-    history: [],
+    coupons: {},
+    quizzes: {},
   } as User,
+});
+
+export const couponsAtom = atom({
+  key: "couponsAtom",
+  default: {} as Coupons,
+});
+
+export const menusAtom = atom({
+  key: "menusAtom",
+  default: {} as Menus,
+});
+
+export const toppingsAtom = atom({
+  key: "toppingsAtom",
+  default: {} as Toppings,
+});
+
+export const quizzesAtom = atom({
+  key: "quizzesAtom",
+  default: {} as quizzes,
+});
+
+export const cartItemsAtom = atom({
+  key: "cartItemsAtom",
+  default: [] as OrderMenu[],
 });
