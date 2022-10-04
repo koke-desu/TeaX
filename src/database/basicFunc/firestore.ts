453063d5513be6e3535a5900da4a0bab9975bebb
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-05 03:36:31 +0900

    firestoreを使った関数を追加

--- src/database/basicFunc/firestore.ts
+++ src/database/basicFunc/firestore.ts
@@ -1,12 +1,51 @@
 //ユーザーのデータをfirestoreから取得したり変更したりする処理をまとめたファイル
-import { doc, getDoc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
-import { User } from "../../type/model";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { Coupons, couponState, User } from "../../type/model";
 import { db } from "../firebase";
 //ユーザーのアカウント情報を取得
-export const getUserData = (id: string): any => {
+export const fetchUserData = (id: string): any => {
   return getDoc(doc(db, "users", `${id}`)).then((doc: any) => {
     console.log("getUserData from firestore", doc.data());
     return doc.data();
+  });
+};
+
+//TODO:関数を試す
+export const fetchToppings = () => {
+  return getDocs(collection(db, "toppings")).then((docs: any) => {
+    console.log("getToppings from firestore", docs.data());
+    return docs.data();
+  });
+};
+
+//TODO:関数を試す
+export const fetchQuizzes = () => {
+  return getDocs(collection(db, "quizzes")).then((docs: any) => {
+    console.log("get quizzes from firestore", docs.data());
+    return docs.data();
+  });
+};
+
+//TODO:関数を試す
+export const fetchMenus = () => {
+  return getDocs(collection(db, "menus")).then((docs: any) => {
+    console.log("getMenus from firestore");
+    return docs.docs();
+  });
+};
+
+//クーポンのステートを変更する関数
+export const changeStateOfCoupon = (userId: string, couponData: Coupons) => {
+  return updateDoc(doc(db, "users", `${userId}`), {
+    coupons: couponData,
   });
 };
 
