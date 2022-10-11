f9e2ca5c39831cf18ef11c42898d884a97c169c1
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-12 03:38:45 +0900

    レイアウトを実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import Layout from "../src/components/Layout";
+import { useInitPage } from "../src/database/authFunc";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </RecoilRoot>
   );
 }
