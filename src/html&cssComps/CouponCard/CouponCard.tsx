b171bb75a111e2754a5116d757ed4a95520211d1
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-15 14:59:23 +0900

    画像が表示されるように修正

--- src/html&cssComps/CouponCard/CouponCard.tsx
+++ src/html&cssComps/CouponCard/CouponCard.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+import { url } from "inspector";
+import Head from "next/head";
 import Image from "next/image";
 import { FC } from "react";
+import image from "/couponCard.png";
 
 type Props = {
   title: string;
@@ -13,73 +16,83 @@ type Props = {
 const CouponCard: FC<Props> = ({ title, description, onClick, isUsable }) => {
   return (
     <>
-      <style>
-        {`
+      <Head>
+        <style>
+          {`
             .all {
-                
-                background-size: contain;
-                width: 331px;
-                height: 80px;
-                font-size: 7px;
-                padding: 8px;
-            }
-            
-            .title {
-                color: black;
-                font-weight: bold;
-                margin: 0 0 0 30px;
-                padding-top: 8px;
-            }
-            
-            .content {
-                color: black;
-                margin: 5px 0 0 50px;
-            }
-            
-            .usecoupon {
-                background-color: #00631e;
-                border-radius: 10px;
-                border: none;
-            }
-            
-            .use {
-                color: white;
-                font-size: 7px;
-            }
-            
-            .a {
-                font-size: 16px;
-            }
-            
-            
-            
-            .b {
-                 /* height: 48px;  */
-                display: flex;
-            
-            }
-            
-            .b-1 {
-                flex-grow:  1;
-                /* overflow: auto; */
-            
-            }
-            
-            .b-2 {
-                white-space: nowrap;
-                margin: 3px;
-            }
-            
-            .move {
-                display:flex;
-                justify-content: center;
-                align-items: center;
-            }
+              position:relative;
+              width: 331px;
+              height: 80px;
+              font-size: 7px;
+              margin: 8px;
+          }
+          .aiueo {
+            position:absolute;
+            width:100%;
+            top:0;
+            z-index:-1;
+          }
+          
+          .title {
+              color: black;
+              font-weight: bold;
+              margin: 0 0 0 30px;
+              padding-top: 8px;
+          }
+          
+          .content {
+              color: black;
+              margin: 5px 0 0 50px;
+          }
+          
+          .usecoupon {
+              background-color: #00631e;
+              border-radius: 10px;
+              border: none;
+          }
+          
+          .use {
+              color: white;
+              font-size: 7px;
+          }
+          
+          .a {
+              font-size: 16px;
+          }
+          
+          
+          
+          .b {
+               /* height: 48px;  */
+              display: flex;
+          
+          }
+          
+          .b-1 {
+              flex-grow:  1;
+              /* overflow: auto; */
+          
+          }
+          
+          .b-2 {
+              white-space: nowrap;
+              margin: 3px;
+          }
+          
+          .move {
+              display:flex;
+              justify-content: center;
+              align-items: center;
+          }
             
         `}
-      </style>
+        </style>
+      </Head>
+
       <>
         <div className="all">
+          <img src="/couponCard.png" className="aiueo" />
+
           <div className="a">
             <p className="title">{title}</p>
           </div>
