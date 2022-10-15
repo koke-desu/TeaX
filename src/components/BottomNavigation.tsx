eb975406dfca455a7f7343102e90ef9f99285373
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-16 07:07:41 +0900

    フッターにアニメーションを追加

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import FooterNav from "../html&cssComps/FooterNav";
 
 const BottomNavigation = () => {
--- src/html&cssComps/FooterNav.tsx
+++ src/html&cssComps/FooterNav.tsx
@@ -1,17 +1,24 @@
 import { css } from "@emotion/css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const FooterNav = () => {
+  const router = useRouter();
+  console.log("path is", router.pathname);
   return (
     <div className={styles.footer}>
       <Link href="/quiz/main">
         <div className={styles.part}>
-          <p className={styles.nazotoki}>謎解き</p>
+          <p className={styles.nazotoki(router.pathname === "/quiz/main")}>
+            謎解き
+          </p>
         </div>
       </Link>
       <Link href="/order/main">
         <div className={styles.part}>
-          <p className={styles.order}>オーダー</p>
+          <p className={styles.order(router.pathname === "/order/main")}>
+            オーダー
+          </p>
         </div>
       </Link>
     </div>
@@ -19,12 +26,16 @@ const FooterNav = () => {
 };
 
 const styles = {
-  nazotoki: css`
-    color: white;
+  nazotoki: (isNazotoki: boolean) => css`
+    transition: all 0.2s ease-out;
+    color: ${isNazotoki ? "white" : "#ebebeb"};
+    font-weight: ${isNazotoki ? "bold" : "none"};
   `,
 
-  order: css`
-    color: white;
+  order: (isOrder: boolean) => css`
+    transition: all 0.2s ease-out;
+    color: ${isOrder ? "white" : "#ebebeb"};
+    font-weight: ${isOrder ? "bold" : "none"};
   `,
 
   part: css`
